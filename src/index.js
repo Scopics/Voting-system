d65ced349d17680188c608330f87cfd2cf3d17ce
MaksGovor <maksgovruha@gmail.com>
2020-11-15 22:27:06 +0200

    Launch eslint

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const pool = new Pool();
 
 const app = express();
 
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.use(express.json());
 app.use(helmet());
 
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+  console.log('Server running on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.json('/');
+  res.json('/');
 });
