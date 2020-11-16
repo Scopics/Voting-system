3f7b59a69f480ddc65dff98bd96f2006292c404a
tedi4t <tedi4t@gmail.com>
2020-11-16 14:43:16 +0200

    adding connection to database

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, 
+    PG_DATABASE, NODE_ENV, DATABASE_URL } = process.env;
+
+const devConfig = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
+const proConfig = DATABASE_URL;
+
+const pool = new Pool({
+    connectionString: NODE_ENV === "production" ? proConfig : devConfig
+});
+
+module.exports = pool;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const { Pool } = require('pg');
 const express = require('express');
 const helmet = require('helmet');
-
-const pool = new Pool();
+const pool = require('./db/db');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM votings ORDER BY start_date DESC')
+    .then(query => {
+      res.json(query.rows);
+    })
+    .catch(err => console.log(err));
 });
 
-app.get('/', (req, res) => {
-  res.json('/');
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
