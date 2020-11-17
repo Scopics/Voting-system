0b4fbdb9ac35ae467722b59c8f01ed843e254541
Anastasiya Trembach <52916015+Anastasia-Tre@users.noreply.github.com>
2020-11-17 14:17:35 +0200

    add routers to server

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 
+const votingRouter = require('./routers/voting.js');
+app.use('/voting', votingRouter);
+
+const userRouteer = require('./routers/user.js');
+app.use('/user', userRouteer);
+
+const petitionRouter = require('./routers/petition.js');
+app.use('/petition', petitionRouter);
+
+const falsificationRouter = require('./routers/falsification.js');
+app.use('/falsification', falsificationRouter);
+
+
 app.get('/', (req, res) => {
   pool.query('SELECT * FROM votings ORDER BY start_date DESC')
     .then(query => {
