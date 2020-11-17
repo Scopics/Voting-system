be007772b0229be0dffca59878c980c86459beac
MaksGovor <maksgovruha@gmail.com>
2020-11-17 18:57:48 +0200

    Add queries

--- src/database/query.sql
+++ src/database/query.sql
@@ -1,5 +1,12 @@
 
 
+--4. Вывод тех петиций которые идут сейчас
+
+
+
+-- Только по дате
+
+SELECT * FROM petitions WHERE (DATE(end_date) >= CURRENT_DATE) AND (DATE(start_date) <= CURRENT_DATE); 
 
 --5. Запрос на добавление новой петиции
 
@@ -34,7 +41,15 @@ SELECT petitions.name, count(petitions.petition_id) AS votes
 
 
 
+--12. Запрос для подсчёта голосов для определенного голосования по вариантам в определенной области в порядке убывания
 
+SELECT vr.variant_id, COUNT(variant_id) AS num_voters 
+    FROM voting_results vr
+    LEFT JOIN users u
+        ON u.user_id=vr.user_id
+    WHERE vr.voting_id = ${voting_id} AND u.district_id = ${district_id}
+    GROUP BY vr.variant_id
+    ORDER by num_voters DESC;
 
 
 --13. Запрос для подсчёта голосов для определенного голосования по вариантам в определенном регионе в порядке убывания
@@ -55,7 +70,22 @@ SELECT variants.name, COUNT(users.name) AS votes
 
 
 
+--14. Запрос для подсчёта голосов за определенную петицию по областям в порядке убывания
 
+
+
+
+SELECT districts.name, count(users.name) AS votes 
+    FROM users
+    INNER JOIN petition_results
+        ON petition_results.user_id = users.user_id
+    INNER JOIN petitions
+        ON petitions.petition_id = petition_results.petition_id
+    INNER JOIN districts
+        ON users.district_id = districts.district_id
+    WHERE petitions.petition_id = 1
+    GROUP BY districts.name
+    ORDER BY votes DESC;
 
 --15. Запрос для подсчёта голосов за определенную петицию по регионам в порядке убывания
 
@@ -77,3 +107,12 @@ SELECT regions.name, count(users.name) AS votes
 
 
 
+
+--16. Запрос на проверку логина и пароля для входа в систему
+
+SELECT * FROM users
+    WHERE users.email = ${user_id} AND users.password = ${password}; 
+
+--18. Запрос на изменение статуса юзера
+
+UPDATE users SET status = ${status} WHERE user_id = ${user_id}; 
