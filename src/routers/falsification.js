b48788d3cd6a09be139b36587f665107bbff4bc5
tedi4t <tedi4t@gmail.com>
2020-12-29 18:37:32 +0200

    changer routes order

--- src/routers/falsification.js
+++ src/routers/falsification.js
@@ -21,22 +21,22 @@ router.post('/create', async (req, res) => {
   }
 });
 
-// get info about specific falsification
-router.get('/:falsification_id', async (req, res) => {
-  const reqData = {
-    req, res,
-    query: queries['Falsification.getInfo'],
-    queryParamsOrder: order['Falsification.getInfo'],
-  };
-  await makeRequest(reqData);
-});
-
 // get all falsifications
 router.get('/all', async (req, res) => {
   const reqData = {
     req, res,
     query: queries['Falsification.getAll'],
     queryParamsOrder: order['Falsification.getAll'],
+  };
+  await makeRequest(reqData);
+});
+
+// get info about specific falsification
+router.get('/:falsification_id', async (req, res) => {
+  const reqData = {
+    req, res,
+    query: queries['Falsification.getInfo'],
+    queryParamsOrder: order['Falsification.getInfo'],
   };
   await makeRequest(reqData);
 });
