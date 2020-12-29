de575711c8c0d4b2e7e48f100a02d03d7adf57fe
tedi4t <tedi4t@gmail.com>
2020-12-29 19:42:16 +0200

    updated user route

--- src/routers/user.js
+++ src/routers/user.js
@@ -56,8 +56,8 @@ router.get('/:user_id', async (req, res) => {
 });
 
 //get data via token
-router.get('/token', async (req, res) => {
-  const token = req.query.token || '';
+router.get('/token/:token', async (req, res) => {
+  const token = req.params.token || '';
   const decodedData = tokenDecoder(token);
   const { email } = decodedData;
   const queryData = {
